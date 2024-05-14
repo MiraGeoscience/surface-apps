912518ddfc080b41f49fd4c744d59d1b2bf6767b
andrewg-mira <89816284+andrewg-mira@users.noreply.github.com>
2024-05-14 09:03:45 -0700

    Initial commit

---
+++ my_app/main.py
@@ -0,0 +1,10 @@
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2022-2024 Mira Geoscience Ltd.                                '
+#                                                                              '
+#  This file is part of my-app package.                                        '
+#                                                                              '
+#  All rights reserved.                                                        '
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+if __name__ == "__main__":  # pragma: no cover
+    print("Hello, world!")
