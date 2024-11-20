759c8f095af12d3c608237afe68510434d6a89e5
Sébastien Hensgen <sebastienh@mirageoscience.com>
2024-11-19 23:20:11 -0500

    add version to the title in doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,9 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
+html_theme_options = {
+    'description': f"version {release}",
+}
 html_static_path = ["_static"]
 
 def get_copyright_notice():
