63dbcf7aab329316bd872b469912bd6014ad4c09
Sébastien Hensgen <sebastienh@mirageoscience.com>
2025-01-12 21:25:00 -0500

    [GEOPY-1933] fix earlier source date to 2024

--- tests/version_test.py
+++ tests/version_test.py
@@ -1,5 +1,5 @@
 # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-#  Copyright (c) 2022-2025 Mira Geoscience Ltd.                                '
+#  Copyright (c) 2024-2025 Mira Geoscience Ltd.                                '
 #                                                                              '
 #  This file is part of surface-apps package.                                  '
 #                                                                              '
