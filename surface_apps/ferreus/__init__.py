553f43df06a61c759ea7ac96c5aaa8cf4b66850b
graeme <graemem@mirageoscience.com>
2026-08-12 11:46:24 -0400

    Add ferreus POC test versus existing method

---
+++ surface_apps/ferreus/__init__.py
@@ -0,0 +1,8 @@
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2026 Mira Geoscience Ltd.                                     '
+#                                                                              '
+#  This file is part of surface-apps package.                                  '
+#                                                                              '
+#  surface-apps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).                 '
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
